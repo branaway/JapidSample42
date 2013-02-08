17d835867961a230fd6d4bd432346223fe95be0f
Bing Ran <bing.ran@gmail.com>
2013-02-09 01:22:06 +0800

    upgrade to play 2.1

--- app/models/Category.java
+++ app/models/Category.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class Category {
 	public String name;
 	public String attr;
-//	public String attr1;
+	public String attr1;
 	public List<Category> subCategories;
 }
--- project/Build.scala
+++ project/Build.scala
@@ -1,28 +1,30 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
     val appName         = "JapidSample42"
-    val appVersion      = "0.7.2"
+    val appVersion      = "0.8.0"
 
     val appDependencies = Seq(
+        javaCore
 //      "japid42" % "japid42_2.9.1" % "0.6.1"
-    "org.apache.commons" % "commons-email" % "1.2"
-    ,"commons-lang" % "commons-lang" % "2.6"
-    ,"org.eclipse.tycho" % "org.eclipse.jdt.core" % "3.8.2.v20120814-155456"
-    ,"com.google.code.javaparser" % "javaparser" % "1.0.8"
+        ,"org.apache.commons" % "commons-email" % "1.2"
+        ,"commons-lang" % "commons-lang" % "2.6"
+        ,"org.eclipse.tycho" % "org.eclipse.jdt.core" % "3.8.2.v20120814-155456"
+        ,"com.google.code.javaparser" % "javaparser" % "1.0.8"
 
     )
 
-
-    val japid = PlayProject("japid42", "0.7.2", appDependencies, path = file("modules/japid42")).settings(
+    val japid = play.Project("japid42", "0.8.0", appDependencies, path = file("modules/japid42")).settings(
     )
 
-    val main = PlayProject(appName, appVersion, Seq(), mainLang = JAVA).settings(
+    val main = play.Project(appName, appVersion, Seq()).settings(
 //      resolvers += "Local Play Repository" at "/Users/bran/projects/playscala/repository/local"
-         resolvers += Resolver.url("Japid on Github", url("http://branaway.github.com/releases/"))(Resolver.ivyStylePatterns)
+      resolvers += Resolver.url("Japid on Github", url("http://branaway.github.com/releases/"))(Resolver.ivyStylePatterns),
+
+      resourceDirectories in (Compile, run) <+= baseDirectory / "japidroot"
 
     ).dependsOn(japid)
 
